fe9c840c5eb66cc41d423edc7bceb68afb24b142
Dmitry Gulitsky <mrdmzver@gmail.com>
2019-01-04 02:33:06 +0300

    Add react-router, react-router-dom

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -1,12 +1,39 @@
 import React, { Component } from 'react'
-import PostList from './posts/PostList'
+
+import {BrowserRouter, Route, Switch, Redirect} from 'react-router-dom'
+
+import DynamicRouteComp from './routingComps/DynamicRouteComp'
+import StaticRouteComp from './routingComps/StaticRouteComp'
+import NotFound from './routingComps/NotFound'
+
 import './App.css'
 
 class App extends Component {
   render () {
+    const loggedIn = true
+    const supportsHistory = 'pushState' in window.history
     return (
-      <div className='App'>
-        <PostList />
+      <div className='App' >
+        <nav className='navbar navbar-light bg-light'>
+          <a className='navbar-brand' href='/'>Navbar</a>
+        </nav>
+        <BrowserRouter forceRefresh={!supportsHistory}>
+          <Switch>
+            <Route exact path='/' component={StaticRouteComp} />
+            <Route exact path='/about' component={StaticRouteComp} />
+            <Route path='/posts/:slug' component={DynamicRouteComp} />
+            <Route component={NotFound} />
+
+            <Route exact path='/user' render={() => (
+              loggedIn === true ? (
+                <Redirect to='/posts/hello-there/' />
+              ) : (
+                <StaticRouteComp />
+              )
+            )} />
+
+          </Switch>
+        </BrowserRouter>
       </div>
     )
   }

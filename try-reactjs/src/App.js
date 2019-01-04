c1ec2af4bbb0541eba59315b01fd2a40b9d8f400
Dmitry Gulitsky <mrdmzver@gmail.com>
2019-01-05 00:35:40 +0300

    Add Dropzone

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -3,7 +3,8 @@ import React, { Component } from 'react'
 // import LifeCycle from './learn/LifeCicle'
 // import Timer from './learn/Timer'
 // import WelcomeHereDialog, {AlertBox, SubItem} from './learn/ParentWrapper'
-import FormsAndInputs from './learn/FormsAndInputs'
+// import FormsAndInputs from './learn/FormsAndInputs'
+import ImgDropAndCrop from './learn/ImgDropAndCrop'
 
 import './App.css'
 
@@ -11,7 +12,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <FormsAndInputs />
+        <ImgDropAndCrop />
       </div>
     )
   }

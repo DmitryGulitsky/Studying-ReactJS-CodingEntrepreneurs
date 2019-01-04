a29b2973ccd680be931264c3c73916417b5045b5
Dmitry Gulitsky <mrdmzver@gmail.com>
2019-01-04 18:16:58 +0300

    Add reusable function

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -1,14 +1,17 @@
 import React, { Component } from 'react'
 // import PostSorting from './posts/PostSorting'
 // import LifeCycle from './learn/LifeCicle'
-import Timer from './learn/Timer'
+// import Timer from './learn/Timer'
+import WelcomeHereDialog, {AlertBox, SubItem} from './learn/ParentWrapper'
+
 import './App.css'
 
 class App extends Component {
   render () {
     return (
       <div className='App'>
-        <Timer startCount='1000' />
+        <AlertBox alertType='danger'>I think this is working...</AlertBox>
+        <WelcomeHereDialog />
       </div>
     )
   }

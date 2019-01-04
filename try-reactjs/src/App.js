cf99e46dd69743d8fc4747d606f84eafa59d968f
Dmitry Gulitsky <mrdmzver@gmail.com>
2019-01-04 18:36:47 +0300

    Add form

--- try-reactjs/src/App.js
+++ try-reactjs/src/App.js
@@ -2,7 +2,8 @@ import React, { Component } from 'react'
 // import PostSorting from './posts/PostSorting'
 // import LifeCycle from './learn/LifeCicle'
 // import Timer from './learn/Timer'
-import WelcomeHereDialog, {AlertBox, SubItem} from './learn/ParentWrapper'
+// import WelcomeHereDialog, {AlertBox, SubItem} from './learn/ParentWrapper'
+import FormsAndInputs from './learn/FormsAndInputs'
 
 import './App.css'
 
@@ -10,8 +11,7 @@ class App extends Component {
   render () {
     return (
       <div className='App'>
-        <AlertBox alertType='danger'>I think this is working...</AlertBox>
-        <WelcomeHereDialog />
+        <FormsAndInputs />
       </div>
     )
   }
